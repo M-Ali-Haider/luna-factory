c7a95a812c9b53176cee5766c85b526aa74ec522
M-Ali-Haider <mah01134212100@gmail.com>
2025-05-25 19:00:29 +0500

    improvement: responsive member details page

--- src/app/(protected)/member-details/page.tsx
+++ src/app/(protected)/member-details/page.tsx
@@ -31,7 +31,7 @@ const Page = async () => {
             ))}
           </div>
           <div className="bg-secondary text-white h-fit w-full p-6 rounded-2xl shadow-lg shadow-black">
-            <h1 className="text-4xl font-extrabold text-primary">
+            <h1 className="text-2xl md:text-4xl font-extrabold text-primary">
               <div>Exclusive</div>
               <div>Memebership</div>
             </h1>
@@ -45,7 +45,7 @@ const Page = async () => {
                 <p className="line-through text-primary text-center">
                   Orignal Price: <span>$39</span>
                 </p>
-                <div className="flex items-center gap-5">
+                <div className="flex flex-wrap items-center gap-5">
                   <div className="text-4xl text-white font-extrabold">now:</div>
                   <h2 className="text-5xl font-extrabold text-primary">
                     ONLY $9.99
@@ -65,7 +65,7 @@ export default Page;
 
 const Heading = ({ text }: { text: string }) => {
   return (
-    <div className="flex items-end justify-between">
+    <div className="flex flex-wrap items-end justify-between">
       <div className="font-medium">{text}</div>
       <div className="flex items-center text-sm text-gray-300">
         <span className="-mt-0.5">READ MORE</span> <ChevronsRight size={16} />
